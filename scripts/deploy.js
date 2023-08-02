fcce80c4e011b7c9ca14f108111f22b153146b84
CodeHead99 <akshitasg08@gmail.com>
2023-08-02 21:12:25 +0530

    initial file structure

---
+++ scripts/deploy.js
@@ -0,0 +1,21 @@
+
+const hre = require("hardhat");
+
+async function main() {
+ 
+
+  const lock = await hre.ethers.deployContract("Lock");
+
+  await lock.waitForDeployment();
+
+  console.log(
+    `Lock with ${ethers.formatEther(
+      lockedAmount
+    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
+  );
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
