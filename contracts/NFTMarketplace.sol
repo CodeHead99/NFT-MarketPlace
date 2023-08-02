626607f1f50aae3877698ff968227a6021049b54
CodeHead99 <akshitasg08@gmail.com>
2023-08-02 23:46:02 +0530

    smart contract completed

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -81,5 +81,99 @@ contract NFTMarketplace is ERC721URIstorage {
     }
 
     //FUNCTION FOR RESALE TOKEN
+    function reSellToken(uint256 tokenId, uint256 price) public payable{
+        require(idMarketItem[tokenId].owner == msg.sender, "only Item owner can perform this operation");
+        require(msg.value == listingPrice, "Price must be equal to listing price");
+        idMarketItem[tokenId].sold = false;
+        idMarketItem[tokenId].price = price;
+        idMarketItem[tokenId].seller = payable(msg.sender);
+        idMarketItem[tokenId].owner = payable(address(this));
 
+        _itmesSold.decrement();
+        _transfer(msg.sender,address(this),tokenId);
+
+    }
+
+    // FUNCTION CREATEMARKETSALE
+
+    function createMarketSale(uint256 tokenId) public payable{
+        uint256 price = idMarketItem[tokenId].price;
+
+        require(msg.value == price, "please submit the asking price in order to complete the process");
+
+        idMarketItem[tokenId].owner = payable(msg.sender);
+        idMarketItem[tokenId].sold = true;
+        idMarketItem[tokenId].owner = payable(address(0));
+
+        _itmesSold.increment();
+        _transfer(address(this), msg.sender, tokenId);
+        payable(owner).transfer(listingPrice);
+        payable(idMarketItem[tokenId].seller).transfer(msg.value);
+    }
+
+    //GETTING UNSOLD NFT DATA
+    function fetchMarketItem() public view returns(MarketItem[] memory){
+        uint256 itemCount = _tokenIds.current();
+        uint256 unSoldItemCount = _tokenIds.current() - _itmesSold.current();
+        uint256 currentIndex = 0;
+
+        MarketItem[] memory items = new MarketItem[](unSoldItemCount);
+        for (uint256 i=0; i<itemCount; i++){
+            if(idMarketItem[i+1].owner == address(this)){
+                uint256 currentId = i+1;
+
+                MarketItem storage currentItem = idMarketItem[currentId];
+                items[currentIndex] = currentItem;
+                currentIndex+=1
+            }
+        }
+        return items;
+    }
+
+    // PURCHASE ITEM
+    function fetchMyNFT() public view returns(MarketItem[] memory){
+        uint256 totalCount = _tokenIds.current();
+        uint256 itemCount = 0;
+        uint256 currentIndex = 0;
+
+        for(uint256 i=0; i<totalCount; i++){
+            if(idMarketItem[i+1].owner == msg.sender){
+                itemCount+=1;
+            }
+        }
+
+        MarketItem[] memory items = new MarketItem[](itemCount);
+        for(uint256 i=0; i<totalCount; i++){
+            if(idMarketItem[i+1].owner == msg.sender){
+            uint256 currentId = i+1;
+            MarketItem storage currentItem = idMarketItem[currentId];
+            items[currentIndex] = currentItem;
+            currentIndex+=1;
+            }
+        }
+        return items;
+    }
+
+    // SINGLE USER ITEM
+    function fetchItemsListed() public view returns(MarketItem[] memory){
+        uint256 totalCount = _tokenIds.current();
+        uint256 itemCount = 0;
+        uint256 currentIndex =0;
+        for(uint256 i=0; i<totalCount; i++){
+            if(idMarketItem[i+1].seller==msg.sender){
+                itemCount +=1;
+
+            }
+        }
+        MarketItem[] memory items = new MarketItem[](itemCount);
+        for(uint256 i=0; i<totalCount; i++){
+            if(idMarketItem[i+1].seller == msg.sender){
+                uint256 currentId = i+1;
+                MarketItem storage currentItem = idMarketItem[currentId];
+                items[currentIndex] = currentItem;
+                currentIndex+=1;
+            }
+        }
+        return items;
+    }
 }
